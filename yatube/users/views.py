dfa1debf4f1873948d3e606232f52c1ee1a7782b
Aleksandr <smirnow-2012@mail.ru>
2022-07-01 11:55:54 +0300

    add users registration and password reset

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -7,3 +7,5 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
+
+
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -117,3 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
